86a559ac4d4c2c7538683f9ebf6d10732ae1d461
Remi Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2024-04-30 15:53:07 -0300

    add ProtConn

---
+++ R/ProtConn.R
@@ -0,0 +1,59 @@
+#' ProtConn
+#'
+#' @param distkm distance matrix in km. Can be created by the 'in_sea_distance()' in this package
+#' @param dkm median dispersal distance in km for the negative exponential dispersal kernal (i.e. `p <- 1-pexp(distkm,log(2)/dkm)`)
+#' @param bioregion sf polygon of the study area (i.e. the sea!)
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' require(MarConsNetData)
+#' bioregion <- get_bioregion()
+#' areas <- get_CPCAD_areas(bioregion,zones=FALSE) |>
+#'   dplyr::mutate(area=sf::st_area(geoms))
+#' distkm <- in_sea_distance(cellsize=100000,bioregion,areas)
+#' PC <- ProtConn(distkm,dkm=100)
+ProtConn <- function(distkm,dkm,bioregion){
+  p <- 1-pexp(distkm,log(2)/dkm)
+  p[p<median(p)/10] <- 0
+  protg <- igraph::graph_from_adjacency_matrix(p,weighted = TRUE)
+
+  edgelist <- data.frame()
+
+  for(f in row.names(distkm)){
+    for(t in colnames(distkm)){
+      # print(paste("dkm = ",dkm," calculating ProtConn for ",f," and ",t))
+
+      if(t==f){
+        pstar=1
+      } else {
+        paths <- igraph::all_simple_paths(protg,
+                                          igraph::V(protg)[which(row.names(distkm)==f)],
+                                          igraph::V(protg)[which(colnames(distkm)==t)],
+                                          cutoff = 4)
+        if(length(paths)<1){
+          pstar=0
+        } else {
+          pstar <- sum(unlist(lapply(paths, function(pa){
+            prod(igraph::E(protg,path=pa)$weight)
+
+          })))
+        }
+      }
+
+
+      edgelist <- dplyr::bind_rows(
+        edgelist,
+        data.frame(to=t,
+                   to_area=areas$area[areas$NAME_E==t],
+                   from=f,
+                   from_area=areas$area[areas$NAME_E==f],
+                   pstar=pstar
+        ) |>
+          dplyr::mutate(product=as.numeric(to_area*from_area*pstar))
+      )
+    }
+  }
+  as.numeric(100*sqrt(sum(edgelist$product))/sum(sf::st_area(bioregion)))
+}
