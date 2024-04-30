ce6467b9557f5552e8c818f6344d480cc7f02106
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2024-04-30 09:11:27 -0300

    package docs

---
+++ R/AllClass.R
@@ -0,0 +1,12 @@
+# vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
+
+#' A Package for Analyzing MPA Data
+#'
+#' The `MarConsNetAnalysis` package is intended to analyze the
+#' real data  that was located and transformed using the
+#' `MarConsNetData` package.
+#'
+#' @importFrom methods new
+#' @name MarConsNetAnalysis-package
+#' @docType package
+NULL
