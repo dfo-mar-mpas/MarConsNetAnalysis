0967eb24dbdf517e372a3e43608dd106b72c9bf5
Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2024-08-29 13:35:02 -0300

    include areas in placeholder function

--- R/ind_placeholder.R
+++ R/ind_placeholder.R
@@ -1,3 +1,5 @@
-ind_placeholder <- function(ind_name = paste0("placeholder_",round(runif(1,1,100)))){
-  data.frame(ind_name = ind_name,ind_value = runif(1,min=0,max=100))
+ind_placeholder <- function(ind_name = paste0("placeholder_",round(runif(1,1,100))),areas,areaName = "NAME_E"){
+  data.frame(area_name = areas[[areaName]],
+             ind_name = ind_name,
+             ind_value = runif(nrow(areas),min=0,max=100))
   }
