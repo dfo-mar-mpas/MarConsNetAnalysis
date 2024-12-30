9d1a491fd462e7dd3698975ba0cca02fefa9593d
Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2024-12-30 15:27:46 -0400

    comma!

--- R/ind_placeholder.R
+++ R/ind_placeholder.R
@@ -14,7 +14,7 @@
 ind_placeholder <- function(ind_name = paste0("placeholder_",round(runif(1,1,100))),areas,areaName = "NAME_E"){
   data.frame(area_name = areas[[areaName]],
              ind_name = ind_name,
-             ind_status = round(100 * rbeta(nrow(areas), shape1=5, shape2=2))
+             ind_status = round(100 * rbeta(nrow(areas), shape1=5, shape2=2)),
              ind_trend = runif(nrow(areas),min=-30,max=30),
              ind_projects = paste(
                sample(c(229,395,480,642,691,835,857,1438,1448,1874,1921,1938,1957,2009,2052,2202,2224,2576,2604,2609),
