17e85632647d6c98efab044b0ba05798e6a96a72
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2025-01-21 08:37:27 -0400

    update to climate analysis

--- R/climate_analysis.R
+++ R/climate_analysis.R
@@ -1,52 +1,67 @@
-# indicators <- climate_change$indicators
-# indicator <- indicators[[11]]
+#' Climate Change Analysis
+#'
+#' This function creates a basic interpretation of the impact of climate change
+#' on indicators. If:
+#' 1. There is a statistically significant change in results between inside
+#' and outside comparison, we determine the change between the two could be a
+#' result of protection efforts in the protected area
+#' 2. There is not a statistically significant change in results between inside
+#' and outside comparison and :
+#'  A) The trend is going in the opposite direction that we would expect and
+#'  statistically significant,we point out that according to our analysis
+#'  climate change could be having the opposite impact than we could expect
+#'  B) The trend is not statistically significant, we  explain that climate
+#'  change does not seem to be having a significant impact on that particular
+#'  indicator and
+#'  C) The trend is going in the direction that we would expect and
+#'  statistically significant,we point out that according to our analysis
+#'  climate change could be having a significant impact on that indicator
+#'
+#' @param trend trend column in indicator_to_plot
+#' @param itp indicator_to_plot data frame, likely from the targets file
+#'
+#' @return a string interpreting results of indicators from a climate change lens
+#' @export
+#'
+#' @examples
+climate_analysis <- function(trend=NULL, itp=indicator_to_plot) {
+  if (!(grepl("BLANK", trend))) {
+    TREND <- str_split(trend, "(?<!\\d)\\.(?!\\d)")[[1]]
+    TREND <- paste0(TREND[1], ". ",trimws(TREND[3], "both"),". ", str_extract(TREND[4], "The difference between.*"))
 
 
-# If the trend is statistically significant we can speak to what direction it is going in
-# we would also indicate the statistical significance.
-
-# If it is not statistically significantly different from the outside border we could
-# conclude that as a whole global warming is playing a role.
-
-## If the results are statistically different than the outter border, we could conclude that
-## it is a result of restriction to fishing effort
-
-
-climate_analysis <- function(indicator) {
-  if (grepl("BLANK", trend)) {
-    return("There is no data for this indicator yet.")
-  } else {
-    # 1. Determine the score of outside
-    keep <- which(tolower(indicator_to_plot$indicators) == tolower(indicator))
-
-    trend <- indicator_to_plot$trend[keep]
-    trend <- sub(".*?(When comparing to outside)", "\\1", trend)
-    actual <- str_extract(trend, "(?<=has shown a )\\w+")
-
-    text <- indicator_to_plot$plot[keep]
-
-    if (!(grepl("dataframe", text))) {
-      paste0(substr(text, 1, nchar(text) - 1), ", dataframe=TRUE)")
+    ioPVAL <- str_extract(trend, "\\d+(\\.\\d+)?(?=\\D*$)")
+    if (ioPVAL < 0.05) {
+      conclusion <- " and therfore implies that protection in this area may directly be impacting this indicator. It is therefore difficult to determine if changes in this indicator are a result of projection
+      efforts or climate change."
     } else {
-      if (grepl("dataframe=FALSE", text)) {
-        text <- gsub("dataframe=FALSE", "dataframe=TRUE", text)
+      # No statistical difference (therefore potentially climate change)
+      conclusion <- " and therefore implies that protection in this area may not be directly impacting this indicator. Trends in this indicator could therefore be linked to climate change"
 
+      if (itp$status_grade[which(tolower(itp$trend) == tolower(trend))] == "A") {
+        # the direction is going in the direction we want and statistically significant
+
+       conclusion <- paste0(conclusion, " The results show us that climate change doesn't seem to be impacting this indicator the way we would expect, and in fact, the trends are showing a statistically significant
+        trend in the opposite direction than we would expect.")
+
+
+
+      } else if (itp$status_grade[which(tolower(itp$trend) == tolower(trend))] %in% c("B", "C", "D")) {
+        # "there is not a significant change"
+
+        conclusion <- paste0(conclusion, " , but the change is not significant.")
+
+
+
+      } else if (itp$status_grade[which(tolower(itp$trend) == tolower(trend))] == "F") {
+        "stat.sig going in the wrong direction"
+        conclusion <- paste0(conclusion, " The results show us that climate change does seem to be impacting this indicator the way we would expect, and in fact, the trends are showing a statistically significant
+        trend in the direction than we would expect as a result of climate change.")
       }
+
     }
-    if (!(grepl("outside", text))) {
-      paste0(substr(text, 1, nchar(text) - 1), ", outside=TRUE)")
-    } else {
-      if (grepl("outside=FALSE", text)) {
-        text <- gsub("outside=FALSE", "outside=TRUE", text)
-
-      }
-    }
-
-    ddff <- eval(parse(text=text))
-
-    desired_direction <- indicator_to_plot$desired_state[keep]
-    pval <- coef(summary(lm(ddff$avg_parameter ~ ddff$year)))["ddff$year", "Pr(>|t|)"]
-
-
+    final <- paste0(TREND, conclusion)
+    return(final)
   }
+
 }
