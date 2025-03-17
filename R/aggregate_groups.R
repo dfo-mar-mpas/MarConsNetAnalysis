4660424d98c49f294a1887fd72456e406ef1fcde
Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2025-03-17 10:48:12 -0300

    better weighting in aggregate_groups

--- R/aggregate_groups.R
+++ R/aggregate_groups.R
@@ -2,22 +2,25 @@
 #'
 #' @param group_level Character string for the name of the group level (e.g. "bin" or "objective")
 #' @param group_name Character string for the name of the group (e.g. "Biomass Metrics" or "Biodiversity")
-#' @param weights numeric vector for the relative weight of each group component
+#' @param weights_ratio numeric vector for the relative weight of each group component (e.g. the ratio of weights between indicator)
+#' @param weights_sum numeric value for the sum of the weights of each group (e.g. the sum of the indicator weights in the bin)
 #' @param ... group components (e.g. the indicator data.frames in the case of bins)
+#'
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' # FIXME placeholder
 #' }
-aggregate_groups <- function(group_level,group_name,weights=1,...){
+aggregate_groups <- function(group_level,group_name,weights_ratio=1,weights_sum=1,...){
+  args <- list(...)
   if(all(unlist(lapply(list(...),function(x) "weight" %in% colnames(x))))){
     df <- bind_rows(...) |>
       mutate({{group_level}} := group_name)
   } else {
     df <- bind_rows(...) |>
       mutate({{group_level}} := group_name,
-             weight = weights)
+             weight = weights_ratio/sum(weights_ratio)/length(args)*weights_sum)
   }
   distinct(df)
 
