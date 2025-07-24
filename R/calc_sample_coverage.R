83a5a54d6a585711bc62bcc525efdd3dd45a7fe0
Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2025-07-24 12:49:00 -0300

    Create calc_sample_coverage.R

---
+++ R/calc_sample_coverage.R
@@ -0,0 +1,46 @@
+#'
+#'
+#' @param x species by sample abundance matrix or dataframe. Rows represent samples and columns represent species.
+#' @param permutations integer number of permutations to calculate sample coverage.
+#'
+#' @returns A list containing the sample coverage matrix, means, and standard deviations.
+#' @export
+#'
+#' @examples
+#' data(BCI,package = "vegan")
+#' SC <- calc_sample_coverage(BCI[1:30,1:50])
+#' ggplot(as.data.frame(SC[-1]), aes(x = N, y = means)) +
+#'   geom_line() +
+#'   geom_ribbon(aes(ymin = means - sd, ymax = means + sd), alpha = 0.2) +
+#'   labs(title = "Sample Coverage", x = "Sample Size", y = "Coverage") +
+#'   theme_minimal()
+#'
+calc_sample_coverage <- function(x, permutations = 100) {
+  result <- list()
+  result$SC <- matrix(NA, nrow = nrow(x), ncol = permutations)
+  result$N <- 1:nrow(x)
+  for (j in 1:ncol(result$SC)) {
+    samples <- x[sample(1:nrow(x), nrow(x), replace = T), ]
+    for (i in 1:nrow(result$SC)) {
+      data <- apply(samples[1:i,], 2, sum, na.rm = TRUE)
+      a1 <- sum(data == 1, na.rm = TRUE)
+      a2 <- sum(data == 2, na.rm = TRUE)
+      N <- sum(data, na.rm = TRUE)
+      s0 <- sum(data > 0, na.rm = TRUE)
+      result$SC[i, j] <- 1 -
+        (a1 / N) * (((N - 1) * a1) / ((N - 1) * a1 + 2 * a2))
+    }
+  }
+
+  result$means <- apply(result$SC, 1, mean, na.rm = TRUE)
+  result$sd <- apply(result$SC, 1, sd, na.rm = TRUE)
+  return(result)
+}
+
+data(BCI,package = "vegan")
+SC <- calc_sample_coverage(BCI[1:30,1:50])
+ggplot(as.data.frame(SC[-1]), aes(x = N, y = means)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = means - sd, ymax = means + sd), alpha = 0.2) +
+  labs(title = "Sample Coverage", x = "Sample Size", y = "Coverage") +
+  theme_minimal()
